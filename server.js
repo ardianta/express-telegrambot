7719d137dbf3dfe33a7027b1abb4268640fb59ab
Glitch (quark-peripheral) <none>
2018-02-11 01:42:33 +0000

    👓🎑 Checkpoint server.js:293729/288

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 // server.js
 // where your node app starts
-
-const TelegramBotClient = require('telegram-bot-client');
-const client = new TelegramBotClient(process.env.TELEGRAM_BOT_TOKEN);
-
-
 // init project
 var express = require('express');
+var request = require('request');
 var app = express();
 
+const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const API_URL = "https://api.telegram.org/bot" + TELEGRAM_TOKEN;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-client.setWebhook('https://quark-peripheral.glitch.me/');
 app.use('/', require('./routes/message'));
 
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  var update = request.body;
+  request.post(API_URL + '/sendMessage')
+        .form({
+              'chat_id': update.message.chat.id,
+              'text': 'Anda mengirim: ' + update.message.text
+    });
 });
 
