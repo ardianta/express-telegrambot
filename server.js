992c43fa8b663c8f53e1bf4cdef4a6f4a79db74e
Glitch (quark-peripheral) <none>
2018-02-11 02:03:28 +0000

    🛳💄 Checkpoint server.js:293729/32

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
@@ -7,10 +9,13 @@ const API_URL = "https://api.telegram.org/bot" + TELEGRAM_TOKEN;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
 app.post("/", function (req, res) {
   
+  res.sendStatus(200);
   var update = req.body;
   request.post(API_URL + '/sendMessage')
         .form({
