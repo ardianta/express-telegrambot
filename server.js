ab5a5e6dc38023bedcbeccf93887b989358157dc
Glitch (quark-peripheral) <none>
2018-02-11 02:14:02 +0000

    🏄🏘 Checkpoint server.js:293729/320 views/index.html:293729/1 package.json:293729/1

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var app = express();
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const API_URL = "https://api.telegram.org/bot" + TELEGRAM_TOKEN;
+const botname = "";
+const botUsername = "";
+const ownerId = 187339848;
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -17,13 +20,18 @@ app.post("/", function (req, res) {
   
   res.sendStatus(200);
   var update = req.body;
-  request.post(API_URL + '/sendMessage')
-        .form({
-          'chat_id': update.message.chat.id,
-          'text': 'Anda mengirim: ' + update.message.text
-    });
+  request.post(API_URL + '/sendMessage').form({
+    'chat_id': update.message.chat.id,
+    'text': 'Anda mengirim: ' + update.message.text
+  });
   
   console.log(req.body);
+});
+
+
+// tampilan halaman home
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
 // listen for requests :)
