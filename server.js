1d184bdae3101c1e2d238bbb3a1d08cccf7d8585
Glitch (quark-peripheral) <none>
2018-02-11 01:32:09 +0000

    📙👼 Checkpoint server.js:293729/281

--- server.js
+++ server.js
@@ -1,70 +1,23 @@
 // server.js
 // where your node app starts
 
+const TelegramBotClient = require('telegram-bot-client');
+const client = new TelegramBotClient(process.env.TELEGRAM_BOT_TOKEN);
+
+
 // init project
 var express = require('express');
-const TelegramBot = require('node-telegram-bot-api');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-const token = 'TELEGRAM_BOT_TOKEN';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+client.setWebhook('https://quark-peripheral.glitch.me/');
+app.use('/', require('./routes/message'));
 
 
-app.get("/webhook", function (request, response) {
-  // Matches "/echo [whatever]"
-  bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-  });
-
-  // Listen for any kind of message. There are different kinds of
-  // messages.
-  bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-  });
-});
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
