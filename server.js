ff7c9c4ad304611b6e5eee2b7329ae8eff6bbbc3
Glitch (quark-peripheral) <none>
2018-02-11 01:53:09 +0000

    🦂🍚 Checkpoint server.js:293729/222 .env:293729/4

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// server.js
-// where your node app starts
-// init project
 var express = require('express');
 var request = require('request');
 var app = express();
@@ -11,15 +8,20 @@ const API_URL = "https://api.telegram.org/bot" + TELEGRAM_TOKEN;
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.use('/', require('./routes/message'));
 
-
-app.get("/", function (req, res) {
-  var update = request.body;
+app.post("/", function (req, res) {
+  
+  var update = req.body;
   request.post(API_URL + '/sendMessage')
         .form({
-              'chat_id': update.message.chat.id,
-              'text': 'Anda mengirim: ' + update.message.text
+          'chat_id': update.message.chat.id,
+          'text': 'Anda mengirim: ' + update.message.text
     });
+  
+  console.log(req.body);
 });
 
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
